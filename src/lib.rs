1fb7bfc051c4bcd395b72dede4f5234184a623b8
Lukasz <lukasz@grochala.lukas@gmail.com>
2018-05-29 21:16:34 +0200

    Wrong query handling
    Fixed compilation problems

--- src/lib.rs
+++ src/lib.rs
@@ -1,95 +1,112 @@
 use std::cmp::max;
-
-pub struct intervalTree {
-    elements: Vec<(isize, isize)>,
-    size: usize,
+use std::time::{SystemTime, UNIX_EPOCH};
+#[derive(Debug)]
+#[derive(Clone)]
+pub struct Node {
+    max: isize,
+    add: isize
 }
 
-impl intervalTree {
-    pub fn new(size: usize) -> intervalTree
+pub struct IntervalTree {
+    elements: Vec<Node>,
+    size: usize,
+    real_size: usize
+}
+
+impl IntervalTree {
+    pub fn new(size: usize) -> IntervalTree
     {
-       let mut size = find_next_pow(size)*2;
-       let mut elements = vec![(0, 0); size];
-       intervalTree {elements, size}
+        let real_size = size;
+        let size = find_next_pow(size)*2;
+        let elements = vec![Node {max: 0, add: 0}; size];
+        IntervalTree {elements, size, real_size}
     }
-    pub fn new_with_vector(el: Vec<isize>) -> intervalTree{
-        let mut size = el.len();
-        let mut size = find_next_pow(size)*2;
-        let mut elements = vec![(0, 0); size];
-        let mut inTree = intervalTree {elements, size};
+    pub fn new_with_vector(el: Vec<isize>) -> IntervalTree{
+        let real_size = el.len();
+        let size = el.len();
+        let size = find_next_pow(size)*2;
+        let elements = vec![Node {max: 0, add: 0}; size];
+        let mut in_tree = IntervalTree {elements, size, real_size};
         for i in 0..el.len() {
-            inTree.add(i, i, el[i]);
+            in_tree.add(i, i, el[i]);
         }
-        inTree
+        in_tree
     }
     pub fn add (&mut self, mut start: usize, mut end: usize, number: isize) {
         if start > end {
             panic!("The start of the interval is bigger than the end.")
         }
+        if end >= self.real_size {
+            panic!("You specified wrong query size.")
+        }
         start += self.size/2;
         end += self.size/2;
         if start != end {
-            self.elements[start].0 += number;
-            self.elements[end].0 += number;
-            self.elements[start].1 += number;
-            self.elements[end].1 += number;
+            self.elements[start].max += number;
+            self.elements[end].max += number;
+            self.elements[start].add += number;
+            self.elements[end].add += number;
             while start/2 != end/2 {
                 if is_left_child (start) {
-                    self.elements[start+1].0 += number;
-                    self.elements[start+1].1 += number;
+                    self.elements[start+1].max += number;
+                    self.elements[start+1].add += number;
                 }
                 if is_right_child (end) {
-                    self.elements[end-1].0 += number;
-                    self.elements[end-1].1 += number;
+                    self.elements[end-1].max += number;
+                    self.elements[end-1].add += number;
                 }
                 start /= 2;
                 end /= 2;
-                self.elements[start].0 = max(self.elements[start*2].0, self.elements[start*2+1].0)+self.elements[start].1;
-                self.elements[end].0 = max(self.elements[end*2].0, self.elements[end*2+1].0)+self.elements[end].1;
+                self.elements[start].max = max(self.elements[start*2].max, self.elements[start*2+1].max)+self.elements[start].add;
+                self.elements[end].max = max(self.elements[end*2].max, self.elements[end*2+1].max)+self.elements[end].add;
             }
             start /= 2;
-            end /= 2;
         }
         else {
-            self.elements[start].0 += number;
-            self.elements[start].1 += number;
+            self.elements[start].max += number;
+            self.elements[start].add += number;
             start /= 2;
         }
         while start != 0 {
-            self.elements[start].0 = max(self.elements[start*2].0, self.elements[start*2+1].0);
+            self.elements[start].max = max(self.elements[start*2].max, self.elements[start*2+1].max)+self.elements[start].add;
             start /= 2;
         }
     }
     
     pub fn find_max (&self, mut start: usize, mut end: usize) -> isize{
+        if start > end {
+            panic!("The start of the interval is bigger than the end.")
+        }
+        if end >= self.real_size {
+            panic!("End of the interval is bigger than interval.")
+        }
         start += self.size/2;
         end += self.size/2;
-        let mut maxStart = self.elements[start].0;
-        let mut maxEnd = self.elements[end].0;
+        let mut max_start = self.elements[start].max;
+        let mut max_end = self.elements[end].max;
         while start/2 != end/2 {
             if is_left_child (start) {
-                maxStart = max(maxStart, self.elements[start+1].0);
+                max_start = max(max_start, self.elements[start+1].max);
             }
             if is_right_child(end) {
-                maxEnd = max (maxEnd, self.elements[end-1].0);
+                max_end = max (max_end, self.elements[end-1].max);
             }
             start /= 2;
             end /= 2;
-            maxStart += self.elements[start].1;
-            maxEnd += self.elements[end].1;
+            max_start += self.elements[start].add;
+            max_end += self.elements[end].add;
         }
         start /= 2;
-        end /= 2;
-        maxStart = max (maxStart, maxEnd);
-        println!("st: {}", maxStart);
+        max_start = max (max_start, max_end);
+        println!("st: {}", max_start);
         while start != 0 {
-            maxStart += self.elements[start].1;
+            max_start += self.elements[start].add;
             start /= 2;
         }
-        maxStart
+        max_start
     }
 
-    fn print_tree (& self) {
+    pub fn print_tree (& self) {
         let mut counter = 1;
         while counter < self.size {
             for i in 0..counter {
@@ -101,8 +118,17 @@ impl intervalTree {
     }
 
 }
-
-
+//insanely bad internet connection so using personal implementation of random
+pub fn random() -> usize
+{
+    let mut number: usize = std::time::SystemTime::now().duration_since(UNIX_EPOCH).unwrap().subsec_nanos() as usize;
+    number *= 15;
+    number %= 12457;
+    number ^= 7;
+    number %= 1001;
+    number *= 1003;
+    number
+}
 
 
 fn is_left_child (number: usize) -> bool {
@@ -128,4 +154,58 @@ fn find_next_pow (number: usize) -> usize {
         result *= 2;
     }
     result
+}
+
+
+struct BruteTree {
+    elements: Vec<isize>
+}
+
+impl BruteTree {
+    fn new (size: usize) -> BruteTree {
+        let elements = vec![0; size];
+        BruteTree {elements}
+    }
+    fn add (&mut self, start: usize, end: usize, number: isize) {
+        for i in start..end+1 {
+            self.elements[i] += number;
+        }
+
+    }
+    fn find_max (&self, start: usize, end: usize) -> isize
+    {
+        let mut max = self.elements[start];
+        for i in start..end+1 {
+            if self.elements[i] > max {
+                max = self.elements[i];
+            }
+        }
+        max
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn check_interval_tree_correctness() {
+        let size = 8;
+        let mut in_tree = IntervalTree::new(size);
+        let mut br_tree = BruteTree::new(size);
+        for _i in 0..10000 {
+            let st = random()%size;
+            let end = random()%(size-st) + st;
+            let number = (random()%10) as isize;
+            println!("Add {} {} {}", st, end, number);
+            in_tree.add(st, end, number);
+            br_tree.add(st, end, number);
+            let st = random()%size;
+            let end = random()%(size-st) + st;
+            println!("Check {} {}", st, end);
+            assert_eq!(in_tree.find_max(st, end), br_tree.find_max(st, end));
+        }
+
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 extern crate intervalTree;
 
 fn main() {
-    let mut inTree = intervalTree::new(8);
-    inTree.add (0, 7, 5);
+    let mut in_tree = intervalTree::IntervalTree::new(8);
+    /*inTree.add (0, 7, 5);
     inTree.print_tree();
     inTree.add (3, 5, 2);
     inTree.print_tree();
-    inTree.add (7, 7, 3);
-    println!("{}", inTree.find_max(2,7));
+    inTree.add (5, 7, 3);
+    println!("{}", inTree.find_max(6,7));*/
+    in_tree.add(3,7,3);
+    in_tree.add(4,4,5);
+    in_tree.print_tree();
+    println!("{}",in_tree.find_max(1,6));
 }
